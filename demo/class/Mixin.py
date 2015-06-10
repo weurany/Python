f4faa5bc7ba2ce15538b1528f7d32df1185e8d98
Huan Wei <weihuan0701@thundersoft.com>
2015-06-10 16:23:31 +0800

    Improve the demo for details.
    Signed-off-by: Huan Wei <weihuan0701@thundersoft.com>

--- demo/class/Mixin.py
+++ demo/class/Mixin.py
@@ -10,17 +10,25 @@ class action(origin_class):
 class mammal(animal):
 	def iswhat(self):
 		print '%s is mammal' % self.name
+	def move(self):
+		print '%s can move' % self.name
 class bird(animal):
 	def iswhat(self):
 		print '%s is bird' % self.name
+	def move(self):
+		print '%s can move' % self.name
 class runable(action):
-	def run(self):
-		print '%s can run' % self.name
-class dog(animal, runable):
 	def iswhat(self):
-		print '%s is a dog' % self.name 
-
-a = dog('Tom')
-a.run()
+		print '%s is something can run' % self.name 
+	def move(self):
+		print '%s can run' % self.name
+class dog1(mammal, runable):
+	pass
+class dog2(runable, mammal):
+	pass
+a = dog1('dog1')
+b = dog2('dog2')
+a.move()
 a.iswhat()
-
+b.move()
+b.iswhat()
